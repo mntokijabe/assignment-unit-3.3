ec61f71cc5964b981461eb6bb4e91ee2328af35b
Elwood Nelson <elwoodnelson@gmail.com>
2024-06-19 15:13:35 -0500

    finished the 5 exercises

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -29,6 +29,8 @@ console.log(removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
+supplyChanges.push(25);
+console.log(supplyChanges);
 
 
 // 6. Create three new variables named 'positives', 'negatives', and
